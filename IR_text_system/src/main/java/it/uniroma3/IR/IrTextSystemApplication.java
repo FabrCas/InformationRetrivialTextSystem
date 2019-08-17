df5f3724de6195aebc4158ed4972b579eb0005d2
Faber <fabriziocasadei97@gmail.com>
2019-08-17 16:44:49 +0100

    Created the project structure

---
+++ IR_text_system/src/main/java/it/uniroma3/IR/IrTextSystemApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.IR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrTextSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrTextSystemApplication.class, args);
+	}
+
+}
