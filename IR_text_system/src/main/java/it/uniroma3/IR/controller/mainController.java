9f7f2d49d74ff0cb1b467a92f1eac896ec52ac30
Faber <fabriziocasadei97@gmail.com>
2019-08-25 16:43:20 +0100

    Added html pages to visualize docs

--- IR_text_system/src/main/java/it/uniroma3/IR/controller/mainController.java
+++ IR_text_system/src/main/java/it/uniroma3/IR/controller/mainController.java
@@ -5,10 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class mainController {
-	
+
+	//vista dei documenti 
 	@RequestMapping(value="/toDoc1")
 	public String toDoc1() {
 		return "DOC1.html";
 	}
+	
+	@RequestMapping(value="/toDoc2")
+	public String toDoc2() {
+		return "DOC2.html";
+	}
+
+	
+	@RequestMapping(value="/toDoc3")
+	public String toDoc3() {
+		return "DOC3.html";
+	}
+
+	
+	@RequestMapping(value="/toDoc4")
+	public String toDoc4() {
+		return "DOC4.html";
+	}
+
 
 }
